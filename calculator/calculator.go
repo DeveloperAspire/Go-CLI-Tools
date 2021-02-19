aafcb2b862e2d4e25b147c5efd24b57216c50167
Franklin <franklinokolie86@gmail.com>
2021-02-19 11:49:58 +0100

    Added calculator logic

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -5,5 +5,46 @@ import (
 )
 
 func main (){
-	fmt.Println("Hello")
+
+	var operation string
+	var number1,number2 int
+	fmt.Print("Enter the First number: ")
+	fmt.Scanln(&number1)
+	fmt.Print("Enter the Second Number: ")
+	fmt.Scanln(&number2)
+	fmt.Print("Enter the Operator( *,/,+,-): ")
+	fmt.Scanln(&operation)
+
+	calculate(number1,number2,operation)
+
+
+}
+
+func calculate(number1,number2 int,operation string){
+
+	var output int
+
+	if(operation == "+"){
+		output = number1 + number2
+		fmt.Println(number1,operation,number2 ,"=", output)
+	} else if( operation == "-"){
+		output = number1 - number2
+		fmt.Println(number1,operation,number2 ,"=", output)
+	}else if( operation == "/"){
+		output = number1 / number2
+		fmt.Println(number1,operation,number2 ,"=", output)
+	}else if( operation == "*"){
+		output = number1 * number2
+	  fmt.Println(number1,operation,number2 ,"=", output)
+	} else{
+		validate(number1,number2,operation)
+	}
+}
+
+func validate(num1, num2 int, operation string){
+	if(num1 == 0 || num2==0 || operation == ""){
+		fmt.Println("Cannot not calculate Empty Input")
+	}else{
+		calculate(num1,num2,operation)
+	}
 }
